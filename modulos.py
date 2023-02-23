f10525f84dfd93fa5845564c5afa3c583158f538
Andres <a---f@outlook.com>
2023-02-23 10:21:29 -0500

    add modulos y operaciones file

---
+++ modulos.py
@@ -0,0 +1,16 @@
+class Calculadora():
+
+    def sumar(self,a,b):
+        return a + b
+
+    def restar(self,a,b):
+        return a - b
+
+    def multiplicar(self,a,b):
+        return a * b
+
+    def dividir(self,a,b):
+        return a / b
+
+    def potencia(self,a,b):
+        return a**b
---
+++ operaciones.py
@@ -0,0 +1,13 @@
+from modulos import Calculadora as c
+
+op = c()
+resultado = op.multiplicar(3,3)
+resultado1 = op.potencia(3,3)
+resultado2 = op.sumar(3,3)
+resultado3 = op.restar(3,3)
+resultado4 = op.dividir(3,3)
+print(resultado)
+print(resultado1)
+print(resultado2)
+print(resultado3)
+print(resultado4)
